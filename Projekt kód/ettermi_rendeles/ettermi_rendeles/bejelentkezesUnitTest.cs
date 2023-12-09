82f126cf94cf9c15c9e09a257fe0ce5ca24d4ba8
mategabor94 <sasko94@gmail.com>
2023-12-09 11:23:13 +0100

    bejelentkezesUnitTest.cs
    create

---
+++ Projekt kód/ettermi_rendeles/ettermi_rendeles/bejelentkezesUnitTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ettermi_rendeles
+{
+    internal class bejelentkezesUnitTest : bejelentkezes
+    {
+        bejelentkezes Bejelentkezes = new bejelentkezes();
+
+
+
+
+    }
+}
